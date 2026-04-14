c537a5b21bd878b9a77b49370d37c54fb73939b6
chriswjohnston <54278456+chriswjohnston@users.noreply.github.com>
2026-04-14 08:33:28 -0400

    Configure passthrough copy for static files
    Added passthrough copy for assets and news.json.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,18 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy({ "src/news.json": "news.json" });
+  return {
+    dir: {
+      input: "src/pages",
+      includes: "../_includes",
+      data: "../_data",
+      output: "_site"
+    },
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+    templateFormats: ["njk", "html", "md"]
+  };
+};module.exports = function (eleventyConfig) {
   
   // Pass through static files
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
